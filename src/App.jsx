07a7323c059e7b847b2831fd24ce479689f12788
jonha20 <jonathan812000@gmail.com>
2025-05-13 21:17:28 +0200

    add/leaflet y 1800 pistas en españa

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,93 @@
-import React, { useState, useEffect } from 'react'
-import basketJSON from '../utils/export.json'
+import React, { useState, useEffect, useRef } from "react";
+import "leaflet/dist/leaflet.css";
 import axios from "axios";
-import './App.css'
+import basketJSON from "../utils/prueba.json";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker } from "react-leaflet/Marker";
+import { Popup } from "react-leaflet/Popup";
 
 function App() {
-  const [pitch, setPitch] = useState([])
+  const [coords, setValue] = useState();
+  const [pitch, setPitch] = useState([]);
 
-  // equivale a un componentDidUpdate()
   useEffect(() => {
-        setPitch(basketJSON.features);
-  }, []); // componentDidUpdate
+    const fetchDefaultCity = async () => {
+      try {
+        const res = await axios.get(
+          `https://ipinfo.io/json?token=${import.meta.env.VITE_LOCATION}`
+        );
+        const data = await res.data.loc;
+        const loc = data.split(",").map(Number);
+        //console.log(loc); // <-- Añade esto para depurar
+        setValue(loc);
+      } catch (error) {
+        console.error("Error fetching location:", error);
+        setValue([40.4168, -3.7038]);
+      }
+    };
+    fetchDefaultCity();
+  }, []);
 
-  //console.log(pitch?.features)
-  
-  
+  useEffect(() => {
+    console.log("basketJSON", basketJSON); // <-- Añade esto para depurar
+    setPitch(basketJSON.features || []);
+  }, []);
+  if (!coords) return null;
+
   return (
     <>
-      
+      <MapContainer
+        className="full-map"
+        center={coords}
+        zoom={13}
+        style={{ height: "100vh", width: "100vw" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {pitch &&
+          pitch.map((feature, idx) => (
+            <Marker
+              key={idx}
+              position={[
+                feature.geometry.coordinates[1],
+                feature.geometry.coordinates[0],
+              ]}
+            >
+              <Popup>
+                <div>
+                  <strong>Campo:</strong>{" "}
+                  {feature.properties.leisure ?? "Sin dato"}
+                  <br />
+                  {feature.properties.surface && (
+                    <>
+                      <strong>Superficie:</strong> {feature.properties.surface}
+                      <br />
+                    </>
+                    
+                  )}
+                  
+                  {feature.properties.covered && (
+                    <>
+                      <strong>Cubierta:</strong> {feature.properties.covered}
+                      <br />
+                    </>
+                  )}
+                  {feature.properties.access && (
+                    <>
+                      <strong>Acceso:</strong> {feature.properties.access}
+                      <br />
+                    </>
+                  )}
+                </div>
+              </Popup>
+            </Marker>
+          ))}
+      </MapContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
